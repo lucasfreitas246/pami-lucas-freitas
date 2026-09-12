1dd84b80854d2fd8eb515d337b61be2e3b790abb
Lucas Freitas <lukasfreitas246@gmail.com>
2026-09-12 19:51:26 -0300

    Aula sobre funções

---
+++ aula 02/funcoes.js
@@ -0,0 +1,35 @@
+/*function MultiplicaNumeros() {
+    let n1 = 2;
+    let n2 = 3;
+
+    console.log(n1 * n2);
+    // + (soma)
+    // - subtração
+    // * (multiplicação)
+    // / (divisão)
+    // ^ (potência)
+    // % (sobra de divisão)
+}
+
+MultiplicaNumeros();
+
+function MultiplicacaocomParametros(v1, v2) {
+    console.log(v1,"x",v2,"=",v1*v2);
+}
+
+MultiplicacaocomParametros(2,5);
+MultiplicacaocomParametros(10,15);
+MultiplicacaocomParametros(200,500);
+
+function EquacaodeBurrinho(x, y, z){
+    let resultado = (x + y) / z;
+    return resultado;                                                  }
+
+
+console.log(EquacaodeBurrinho(2,3,5));*/
+
+const EquacaodeTontinho = (x,y,z,w) => {
+    return resultado = (x + y) * (z + w) / 2;
+}
+    
+console.log("O resultado é: ", EquacaodeTontinho(3, 5, 8, 13));
--- aula 03-09/matematica.js
+++
@@ -1,4 +0,0 @@
-let v1 = 2;
-let v2 = 3;
-
-console.log(v1 + v2);
