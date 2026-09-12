dedb362d566d98e22f44ff6254f9202b5263d29e
Lucas Freitas <lukasfreitas246@gmail.com>
2026-09-12 19:47:23 -0300

    Cria arquivo que demonstra calculos

---
+++ aula01/introducao.js
@@ -0,0 +1,22 @@
+console.log("Falae Seus Otário");
+
+//criação de variaveis
+var Variavel01="Oi,";
+let Variavel02="Tudo bem?";
+const Variavel03="Vocês são lindos";
+
+//concatenação de variaveis
+console.log(Variavel01+Variavel02);// sem espaços 
+console.log(Variavel01,Variavel02);// insere espaços
+
+Variavel01="Olá!";
+console.log(Variavel01, Variavel02);
+
+let Teste = "texto";
+console.log(typeof(Teste));
+Teste = 2;
+console.log(typeof(Teste));
+Teste={nome: "João", idade: 38};
+console.log("O tipo agora é: ", typeof(Teste));
+Teste=[1, 2];
+console.log("O resultado do tipo de array é", typeof(Teste));
